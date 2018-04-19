efc6848e6b1a03fcf6458ffcc548710f15b0e875
Pavel Savchik <p.savchik@gmail.com>
2018-04-19 23:17:40 +0300

    Added request headers.

---
+++ src/main/java/com/getbux/constants/Headers.java
@@ -0,0 +1,26 @@
+package com.getbux.constants;
+
+public class Headers {
+    
+    public static final String AUTH_HEADER_NAME = "Authorization";
+
+    public static final String AUTH_HEADER_VALUE = "Bearer " +
+            "eyJhbGciOiJIUzI1NiJ9.eyJyZWZyZXNoYWJsZSI6ZmFsc2UsInN1YiI6ImJiMGNkYTJiLWE" +
+            "xMGUtNGVkMy1hZDVhLTBmODJiNGMxNTJjNCIsImF1ZCI6ImJldGEuZ2V0YnV4LmNvbSIsInN" +
+            "jcCI6WyJhcHA6bG9naW4iLCJydGY6bG9naW4iXSwiZXhwIjoxODIwODQ5Mjc5LCJpYXQiOjE" +
+            "1MDU0ODkyNzksImp0aSI6ImI3MzlmYjgwLTM1NzUtNGIwMS04NzUxLTMzZDFhNGRjOGY5MiI" +
+            "sImNpZCI6Ijg0NzM2MjI5MzkifQ.M5oANIi2nBtSfIfhyUMqJnex-JYg6Sm92KPYaUL9GKg";
+
+    public static final String LANGUAGE_HEADER_NAME = "Accept-Language";
+
+    public static final String LANGUAGE_HEADER_VALUE = "nl-NL,en;q=0.8";
+
+    public static final String CONTENT_TYPE_HEADER_NAME = "Content-Type";
+
+    public static final String CONTENT_TYPE_HEADER_VALUE = "application/json";
+
+    public static final String ACCEPT_HEADER_NAME = "Accept";
+
+    public static final String ACCEPT_HEADER_VALUE = "application/json";
+
+}
